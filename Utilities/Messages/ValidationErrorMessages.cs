13c5358130d7bac19aadfa06988dba3270b6174d
Hien Nguyen <hnguyen110.akc@gmail.com>
2021-12-28 21:38:15 -0500

    Create the add contact information feature, this feature will be tested when the sign up API is in progress.

--- Utilities/Messages/ValidationErrorMessages.cs
+++ Utilities/Messages/ValidationErrorMessages.cs
@@ -5,4 +5,5 @@ public static class ValidationErrorMessages
     public const string Required = "Required";
     public const string MinimumLength = "Minimum length";
     public const string MaximumLength = "Maximum length";
+    public const string EmailAddress = "Invalid email address";
 }
