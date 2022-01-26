973d5438a82997b0621ba4aa1cd66a3e9689122c
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-01-26 18:46:17 -0500

    Clean up the controller of retrieving the profile picture.

--- Controllers/ProfileController.cs
+++ Controllers/ProfileController.cs
@@ -7,10 +7,9 @@ namespace API.Controllers;
 public class ProfileController : BaseController
 {
     [HttpGet("profile-picture")]
-    public async Task<string> RetrieveProfilePicture(string? id)
+    public async Task<string?> RetrieveProfilePicture(string? id)
     {
-        var path = await Mediator!.Send(new RetrieveProfilePicture.Query {Id = id});
-        return path == null ? "" : Convert.ToBase64String(await System.IO.File.ReadAllBytesAsync(path));
+        return await Mediator!.Send(new RetrieveProfilePicture.Query {Id = id});
     }
 
     [HttpGet("profile-information")]
--- Handlers/Profile/RetrieveProfilePicture.cs
+++ Handlers/Profile/RetrieveProfilePicture.cs
@@ -10,12 +10,12 @@ namespace API.Handlers.Profile;
 
 public class RetrieveProfilePicture
 {
-    public class Query : IRequest<string?>
+    public class Query : IRequest<string>
     {
         public string? Id { get; set; }
     }
 
-    public class Handler : IRequestHandler<Query, string?>
+    public class Handler : IRequestHandler<Query, string>
     {
         private readonly ICredentialAccessor _accessor;
         private readonly IAuthorization _authorization;
@@ -28,26 +28,34 @@ public class RetrieveProfilePicture
             _authorization = authorization;
         }
 
-        public async Task<string?> Handle(Query request, CancellationToken cancellationToken)
+        public async Task<string> Handle(Query request, CancellationToken cancellationToken)
         {
+            string? path = null;
             if (string.IsNullOrWhiteSpace(request.Id))
-                return await _repository
+            {
+                path = await _repository
                     .RetrieveProfilePictureByAccount(
                         _accessor.RetrieveAccountId(),
                         cancellationToken
                     );
-
-            var isAdmin = await _authorization.IsAdministrator();
-            if (!isAdmin)
-                throw new ApiException(
-                    HttpStatusCode.Unauthorized,
-                    ApiErrorMessages.Unauthorized
-                );
-            return await _repository
-                .RetrieveProfilePictureByAccount(
-                    request.Id,
-                    cancellationToken
-                );
+            }
+            else
+            {
+                var isAdmin = await _authorization.IsAdministrator();
+                if (!isAdmin)
+                    throw new ApiException(
+                        HttpStatusCode.Unauthorized,
+                        ApiErrorMessages.Unauthorized
+                    );
+                path = await _repository
+                    .RetrieveProfilePictureByAccount(
+                        request.Id,
+                        cancellationToken
+                    );
+            }
+            return path == null
+                ? ""
+                : Convert.ToBase64String(await File.ReadAllBytesAsync(path, cancellationToken));
         }
     }
 }
