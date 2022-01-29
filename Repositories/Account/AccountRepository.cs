3b2df7c4daab6767b57ed1a2d05924a750e57e02
Hien Nguyen <hnguyen110.akc@gmail.com>
2022-01-29 12:40:02 -0500

    Experiment the change password feature

--- Repositories/Account/AccountRepository.cs
+++ Repositories/Account/AccountRepository.cs
@@ -1,4 +1,8 @@
+using System.Net;
 using API.DatabaseContext;
+using API.Exceptions;
+using API.Utilities.Messages;
+using API.Utilities.Security;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Repositories.Account;
@@ -6,10 +10,12 @@ namespace API.Repositories.Account;
 public class AccountRepository : IAccountRepository
 {
     private readonly Context _database;
+    private readonly ISecurity _security;
 
-    public AccountRepository(Context database)
+    public AccountRepository(Context database, ISecurity security)
     {
         _database = database;
+        _security = security;
     }
 
     public async Task Save(Models.Account account, CancellationToken cancellationToken)
@@ -24,5 +30,32 @@ public class AccountRepository : IAccountRepository
             .Account
             !.FirstOrDefaultAsync(e => e.Username == username, cancellationToken);
         return record;
+    }
+
+    public Task<Models.Account?> FindById(string id, CancellationToken cancellationToken)
+    {
+        var record = _database
+            .Account
+            !.FirstOrDefaultAsync(e => e.Id == id, cancellationToken);
+        return record;
+    }
+
+    public async Task ChangePassword(string id, string oldPassword, string newPassword,
+        CancellationToken cancellationToken)
+    {
+        var record = await FindById(id, cancellationToken);
+        if (record == null)
+            throw new ApiException(
+                HttpStatusCode.NotFound,
+                ApiErrorMessages.NotFound
+            );
+        var passwordMatched = _security.Compare(oldPassword, record.PasswordSalt, record.PasswordHash);
+        if (!passwordMatched)
+            throw new ApiException(
+                HttpStatusCode.Unauthorized,
+                ApiErrorMessages.Unauthorized
+            );
+        record.PasswordHash = _security.CreatePasswordHash(newPassword, record.PasswordSalt);
+        await _database.SaveChangesAsync(cancellationToken);
     }
 }
