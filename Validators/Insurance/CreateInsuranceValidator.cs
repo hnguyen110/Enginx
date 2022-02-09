1c33470c935b2009fe495034efd468516eb5481a
Minsu Kim <dev.minsukim@gmail.com>
2022-02-09 22:53:39 +0900

    Add validator for creating insurance

---
+++ Validators/Insurance/CreateInsuranceValidator.cs
@@ -0,0 +1,29 @@
+using API.Handlers.Insurance;
+using API.Utilities.Messages;
+using FluentValidation;
+
+namespace API.Validators.Insurance;
+
+public class CreateInsuranceValidator : AbstractValidator<CreateInsurance.Command>
+{
+    public CreateInsuranceValidator()
+    {
+        RuleFor(e => e.Name)
+            .NotNull()
+            .WithMessage(ValidationErrorMessages.Required)
+            .NotEmpty()
+            .WithMessage(ValidationErrorMessages.Required);
+        
+        RuleFor(e=>e.Price)
+            .NotNull()
+            .WithMessage(ValidationErrorMessages.Required)
+            .NotEmpty()
+            .WithMessage(ValidationErrorMessages.Required);
+        
+        RuleFor(e=>e.Description)
+            .NotNull()
+            .WithMessage(ValidationErrorMessages.Required)
+            .NotEmpty()
+            .WithMessage(ValidationErrorMessages.Required);
+    }
+}
