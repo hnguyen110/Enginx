3e78e6da65be00dcf760ccabab3a0495144c4b8b
Reza Poursafa <rezapscodes@gmail.com>
2022-03-10 18:51:38 -0500

    Fix Upload vehicle picture not throwing a 404 exception

--- Handlers/Vehicle/RetrieveVehiclePictures.cs
+++ Handlers/Vehicle/RetrieveVehiclePictures.cs
@@ -32,8 +32,6 @@ public class RetrieveVehiclePictures
                     request.Id,
                     cancellationToken
                 );
-            if (!records.Any())
-                throw new ApiException(HttpStatusCode.NotFound, ApiErrorMessages.NotFound);
             return records;
         }
     }
