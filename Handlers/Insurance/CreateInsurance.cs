e78a4db1a8c75daaf6b3c15cbe2c3b6e7fcab308
Reza Poursafa <rezapscodes@gmail.com>
2022-03-10 18:55:48 -0500

    Fix Create Insurance for returning the insuarnce

--- Handlers/Insurance/CreateInsurance.cs
+++ Handlers/Insurance/CreateInsurance.cs
@@ -1,33 +1,38 @@
 using System.Net;
+using API.DTOs.Insurance;
 using API.Exceptions;
 using API.Repositories.Insurance;
 using API.Utilities.Messages;
 using API.Utilities.Security;
+using AutoMapper;
 using MediatR;
 
 namespace API.Handlers.Insurance;
 
 public class CreateInsurance
 {
-    public class Command : IRequest<Unit>
+    public class Command : IRequest<RetrieveAllInsurancesDTO>
     {
         public string? Name { get; set; }
         public string? Description { get; set; }
         public double Price { get; set; }
     }
 
-    public class Handler : IRequestHandler<Command, Unit>
+    public class Handler : IRequestHandler<Command, RetrieveAllInsurancesDTO>
     {
         private readonly IAuthorization _authorization;
         private readonly IInsuranceRepository _repository;
+        private readonly IMapper _mapper;
 
-        public Handler(IInsuranceRepository repository, IAuthorization authorization)
+
+        public Handler(IInsuranceRepository repository, IAuthorization authorization , IMapper mapper)
         {
             _repository = repository;
             _authorization = authorization;
+            _mapper = mapper;
         }
 
-        public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+        public async Task<RetrieveAllInsurancesDTO> Handle(Command request, CancellationToken cancellationToken)
         {
             var isAdmin = await _authorization.IsAdministrator();
             if (!isAdmin)
@@ -43,7 +48,7 @@ public class CreateInsurance
                 Price = request.Price
             };
             await _repository.Save(insurance, cancellationToken);
-            return Unit.Value;
+            return _mapper.Map<Models.Insurance , RetrieveAllInsurancesDTO>(insurance);;
         }
     }
 }
